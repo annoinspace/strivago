09bcae1e11cd337086840d376b25a1ddf64dcfd4
annoinspace <aneesahalmask@gmail.com>
2023-02-10 11:37:37 +0000

    user routes created with register and login

--- src/api/users/index.js
+++ src/api/users/index.js
@@ -1,6 +1,97 @@
 import express from "express"
 import UsersModel from "./model.js"
+import { jwtAuthMiddleware } from "../../lib/jwtAuth.js"
+import { hostOnlyMiddleware } from "../../lib/HostOnly.js"
+import { createAccessToken } from "../../lib/tools.js"
+import createHttpError from "http-errors"
 
 const usersRouter = express.Router()
+
+usersRouter.post("/register", async (req, res, next) => {
+  try {
+    // make sure an email is not aready in use
+    const existingUser = await UsersModel.findOne({ email: req.body.email })
+    if (existingUser) {
+      next(createHttpError(400, "Email already in use"))
+    } else {
+      const newUser = new UsersModel(req.body)
+      const newUserSaved = await newUser.save()
+      if (newUserSaved) {
+        res.status(201).send(newUserSaved)
+      } else {
+        res.status(400).send({ message: "something went wrong creating new user" })
+      }
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
+usersRouter.post("/login", async (req, res, next) => {
+  try {
+    const { email, password } = req.body
+    const user = await UsersModel.checkCredentials(email, password)
+    if (user) {
+      console.log("user", user)
+      const payload = { _id: user._id, role: user.role }
+      const accessToken = await createAccessToken(payload)
+      res.status(201).send({ accessToken, message: "user can now login" })
+    } else {
+      next(createHttpError(401, "login unsuccessful"))
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
+usersRouter.get("/", jwtAuthMiddleware, async (req, res, next) => {
+  try {
+    const users = await UsersModel.find({})
+    res.send(users)
+  } catch (error) {
+    next(error)
+  }
+})
+
+usersRouter.get("/me", jwtAuthMiddleware, async (req, res, next) => {
+  try {
+    if (req.user) {
+      const user = await UsersModel.findById(req.user._id)
+      res.send(user)
+    } else {
+      res.status(400).send({ message: "something went getting your profile" })
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
+usersRouter.get("/me/accomodation", jwtAuthMiddleware, hostOnlyMiddleware, async (req, res, next) => {
+  try {
+    const user = request.user
+    const accomodation = await UsersModel.find({ users: user._id })
+    res.send(accomodation)
+  } catch (error) {
+    next(error)
+  }
+})
+
+usersRouter.delete("/:userId", jwtAuthMiddleware, async (req, res, next) => {
+  try {
+    const userId = request.params.userId
+    if (userId === request.user._id) {
+      const userToDelete = await User.findByIdAndDelete(userId)
+      if (userToDelete) {
+        res.status(204).send({ message: "user deleted" })
+      } else {
+        next(createHttpError(404, `User with id ${userId} not found!`))
+      }
+    } else {
+      next(createHttpError(401, `You can't delete another user :O`))
+    }
+  } catch (error) {
+    next(error)
+  }
+})
 
 export default usersRouter
