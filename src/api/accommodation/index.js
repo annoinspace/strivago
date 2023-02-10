c93d861574297aea670dc098ee22b967d49f0f77
annoinspace <aneesahalmask@gmail.com>
2023-02-10 14:20:13 +0000

    delete and edit added for accom

--- src/api/accommodation/index.js
+++ src/api/accommodation/index.js
@@ -43,4 +43,46 @@ accommodationRouter.get("/:accommId", jwtAuthMiddleware, hostOnlyMiddleware, asy
   }
 })
 
+accommodationRouter.put("/:accommId", jwtAuthMiddleware, hostOnlyMiddleware, async (req, res, next) => {
+  try {
+    const accomId = req.params.accommId
+
+    const specificAccom = await AccommodationsModel.findById(accomId)
+    const accomOwner = specificAccom.host.toString()
+    const userMakingRequest = req.user._id
+
+    if (specificAccom) {
+      if (userMakingRequest === accomOwner) {
+        console.log("user and host match")
+
+        const updatedAccom = await AccommodationsModel.findByIdAndUpdate(accomId, req.body, { new: true })
+        console.log("updated Accom", updatedAccom)
+        res.status(200).send(updatedAccom)
+      } else {
+        res.status(403).send({ message: "user and host don't match" })
+      }
+    } else {
+      res.status(404).send({ message: "accommodation does not exist" })
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
+accommodationRouter.delete("/:accommId", jwtAuthMiddleware, hostOnlyMiddleware, async (req, res, next) => {
+  try {
+    const accomId = req.params.accommId
+    const specificAccom = await AccommodationsModel.findById(accomId)
+
+    if (!specificAccom) return res.status(404).send({ message: "accommodation does not exist" })
+    if (req.user._id !== specificAccom.host.toString())
+      return res.status(403).send({ message: "user and host don't match" })
+
+    await AccommodationsModel.findByIdAndDelete(accomId)
+    res.status(204).send()
+  } catch (error) {
+    next(error)
+  }
+})
+
 export default accommodationRouter
